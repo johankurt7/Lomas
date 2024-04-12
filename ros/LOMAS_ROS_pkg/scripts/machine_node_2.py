729aa8b785106ef0bec8b48ce670f6db09944dc4
johan <johkuh211@edunet.oru.se>
2024-04-12 15:56:14 +0200

    + sendGCodeCmd + stopCallback + abortCallback + intervallCallback + cmdCallback

--- ros/LOMAS_ROS_pkg/scripts/machine_node_2.py
+++ ros/LOMAS_ROS_pkg/scripts/machine_node_2.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# license removed for brevit
 import rclpy
 from rclpy.node import Node
 import serial
@@ -47,30 +49,45 @@ class MachineNode(Node):
 
         self.pubMachineStatus.publish(self.status)
 
+    def removeComment(string):
+        if (string.find(';') == -1):
+            return string
+        else:
+            return string[:string.index(';')]
+
     def connectToMachine(self):
-        try:
-            self.ser = serial.Serial(self.port, 115200)
-            self.ser.write(b"\r\n\r\n")  # Hit enter a few times to wake the Printrbot
-            time.sleep(2)  # Wait for machine to initialize
-            self.ser.flushInput()  # Flush startup text in serial input
+
+        self.get_logger().info('Opening Serial Port')
+        if self.IsInSimMode:
+            self.get_logger().info('Warning : Serial port will be simulated')
             self.status.ErrorNr = 0
-            self.get_logger().info('Serial port connected to machine')
-        except serial.SerialException:
-            self.status.ErrorNr = 98
-            self.get_logger().error('Error when opening Serial Port')
+        else:
+            try:
+                self.s = serial.Serial(self.port, 115200)
+                self.s.write(b"\r\n\r\n")  # Hit enter a few times to wake the robot
+                time.sleep(2)  # Wait for machine to initialize
+                self.s.flushInput()  # Flush startup text in serial input
+                self.status.ErrorNr = 0
+                self.get_logger().info('Serial port connected to machine')
+            except serial.SerialException:
+                self.status.ErrorNr = 98
+                self.get_logger().error('Error when opening Serial Port')
 
         self.pubMachineStatus.publish(self.status)
 
     def sendSerialCmd(self, cmd):
         if self.IsInSimMode:
-            grbl_out = b'oMGok\n'
+            grbl_out = 'oMGok\n'
         else:
             self.ser.write(cmd)  # Send g-code block
             grbl_out = self.ser.readline()  # Wait for response with carriage return
 
         self.get_logger().info(f': {grbl_out.strip()}')
 
-        return grbl_out == b'oMGok\n'
+        if grbl_out == 'oMGok\n':
+            return True
+        else:
+            return False
 
     def sendGCodeFile(self, file, seq):
         self.status.SequensStarted = True
@@ -86,7 +103,7 @@ class MachineNode(Node):
                     l = self.remove_comment(line)
                     l = l.strip()  # Strip all EOL characters for streaming
                     if not l.isspace() and len(l) > 0:
-                        ok = self.send_serial_cmd((l + '\n').encode())
+                        ok = self.sendSerialCmd((l + '\n'))
 
                     if self.stop:
                         self.get_logger().info('Stopped')
@@ -99,6 +116,7 @@ class MachineNode(Node):
                                 self.get_logger().info('Aborting while')
                                 self.pubMachineStatus.publish(self.status)
                                 break
+
                             time.sleep(0.1)
 
                         self.get_logger().info('Restarted')
@@ -115,6 +133,7 @@ class MachineNode(Node):
         except FileNotFoundError:
             self.get_logger().error(f"Error: File {file} not found")
 
+        # f.close()????
         self.status.ErrorNr = 0
         self.status.SequensStarted = False
         self.status.MachineMoving = False
@@ -122,24 +141,77 @@ class MachineNode(Node):
         self.abort = False
 
     def sendGCodeCmd(self, cmd):
+        self.status.SequensStarted = True
+        self.status.MachineMoving = True
+        self.status.SequenseNr = 99
+        self.pubMachineStatus.publish(self.status)
+
+        ok = self.send_serial_cmd((cmd + '\n'))
+
+        if ok:
+            self.status.ErrorNr = 0
+            self.status.SequensStarted = False
+            self.status.MachineMoving = False
+            self.status.SequenseNr = 0
+
+        #self.status_publisher.publish(self.status)
 
     def stopCallback(self, data):
+        self.get_logger().info('Stop')
+        self.stop = data.data
 
     def abortCallback(self, data):
+        self.get_logger().info('Abort')
+        self.abort = data.data
 
     def intervallCallback(self, data):
+        self.get_logger().info(f'Set interval: {data.data}')
+        self.status.Interval = data.data
+        self.setParameters([rclpy.parameter.Parameter('cultivation_interval', rclpy.Parameter.Type.INTEGER, data.data)])
+        self.pubMachineStatus.publish(self.status)
 
     def cmdCallback(self, data):
+        self.get.logger().info(f'Received command: {data.data}')
+        if data.data == 99:
+            self.get.logger().info('Starting to home robot')
+            self.status.IsSynced = False
+            Self.sendGCodeCmd('G28 X Y Z')
+            self.status.IsSynced = True
+        elif data.data == 1:
+            self.get.logger().info('Send cultivation.g file')
+            self.status.SequenseNr = 1
+            self.sendGCodeFile(path + 'cultivation.g', 1)
+        elif data.data == 2:
+            self.get.logger().info('Send seed.g file')
+            self.status.SequenseNr = 2
+            self.sendGCodeFile(path + 'seed.g', 2)
+        elif data.data == 90:
+            self.get.logger().info('Man. pos X')
+            self.sendGCodeCmd('G91\n'+'G0 X10 F1000\n')
+        elif data.data == 91:
+            self.get.logger().info('Man. neg X')
+            self.sendGCodeCmd('G91\n'+'G0 X-10 F1000\n')
+        elif data.data == 92:
+            self.get.logger().info('Man. pos Y')
+            self.sendGCodeCmd('G91\n'+'G0 Y10 F1000\n')
+        elif data.data == 93:
+            self.get.logger().info('Man. neg Y')
+            self.sendGCodeCmd('G91\n'+'G0 Y-10 F1000\n')
+        elif data.data == 94:
+            self.get.logger().info('Man. pos X pos Y')
+            self.sendGCodeCmd('G91\n'+'G0 X10 Y10 F1000\n')
+        elif data.data == 95:
+            self.get.logger().info('Man. neg X pos Y')
+            self.sendGCodeCmd('G91\n'+'G0 X-10 Y10 F1000\n')
+        elif data.data == 96:
+            self.get.logger().info('Man. pos X neg Y')
+            self.sendGCodeCmd('G91\n'+'G0 X10 Y-10 F1000\n')
+        elif data.data == 97:
+            self.get.logger().info('Man. neg X neg Y')
+            self.sendGCodeCmd('G91\n'+'G0 X-10 Y-10 F1000\n')
 
+        self.pubMachineStatus.publish(self.staus)
 
-
-'''
-def removeComment(string):
-    if (string.find(';')==-1):
-        return string
-    else:
-        return string[:string.index(';')]
-'''
 
 
 def main(args=None):
